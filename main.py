eeb723c2d10050d840a072814fca5f6c2fae31ae
m-peter-g <m.p.gergo@gmail.com>
2026-08-05 17:37:35 +0200

    XML now live
    Works like a... crooked and stupid charm. Still got to tinker with this one but it doesn't crash, so gold star for me!

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
 import time
+import xml.etree.ElementTree as ET
 
 #Time is system clock by default, can be set manually via the set_time function
 current_time = time.strftime("%H:%M")
+
+#XML file builder function
+def build_xml_file():
+    floor = ["A", "B", "C", "D"]    
+    lots_per_floor = ["-2", "-1", "1", "2", "3",
+                      "4", "5", "6", "7", "8",
+                      "9", "10", "11", "12", "13",
+                      "14", "15", "16", "17", "18",
+                      "19", "20", "21", "22", "23"]
+
+    garage = ET.Element("garage")
+    lots = ET.SubElement(garage, "lots")
+    for i in range(len(floor)):
+        for j in range(len(lots_per_floor)):
+            if j < 2:
+                lot = ET.SubElement(lots, "lot", id=f"{floor[i]}{lots_per_floor[j]}", disabled="true")
+                bookings = ET.SubElement(lot, "bookings")
+            else:    
+                lot = ET.SubElement(lots, "lot", id=f"{floor[i]}{lots_per_floor[j]}", disabled="false")
+                bookings = ET.SubElement(lot, "bookings")
+    meta = ET.SubElement(garage, "metadata")
+    next_booking_id = ET.SubElement(meta, "next_booking_id")
+    next_booking_id.text = "1"
+    current_time_element = ET.SubElement(meta, "current_time")
+    current_time_element.text = current_time
+    tree = ET.ElementTree(garage)
+    tree.write("data.xml")
 
 #Set time function
 def set_time():
@@ -30,6 +58,14 @@ def display_menu():
 
     choice = input("Enter your choice (1-5): ")
     return choice
+
+#If the XML file does not exist, create it
+try:
+    with open("data.xml", "r") as file:
+        pass
+except FileNotFoundError:
+    build_xml_file()
+
 
 #Call menu, handle choice
 while True:
