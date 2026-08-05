b788d815d952ef5fe9f6809991e0137623ba7554
m-peter-g <m.p.gergo@gmail.com>
2026-08-05 18:16:08 +0200

    Some redundancy fixes
    Huzzah! Now the XML is actually readable by human eyes.
    Also completely moved the time keeping to the XML. Will probably have to tweak that later. But I totally didn't screw up the parsing, no sir!

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import time
 import xml.etree.ElementTree as ET
 
-#Time is system clock by default, can be set manually via the set_time function
-current_time = time.strftime("%H:%M")
-
 #XML file builder function
 def build_xml_file():
     floor = ["A", "B", "C", "D"]    
@@ -23,23 +20,28 @@ def build_xml_file():
             else:    
                 lot = ET.SubElement(lots, "lot", id=f"{floor[i]}{lots_per_floor[j]}", disabled="false")
                 bookings = ET.SubElement(lot, "bookings")
-    meta = ET.SubElement(garage, "metadata")
+    meta = ET.SubElement(garage, "meta")
     next_booking_id = ET.SubElement(meta, "next_booking_id")
     next_booking_id.text = "1"
     current_time_element = ET.SubElement(meta, "current_time")
-    current_time_element.text = current_time
+    current_time_element.text = time.strftime("%H:%M")
     tree = ET.ElementTree(garage)
+    ET.indent(tree, space="\t", level=0) #for pretty printing
     tree.write("data.xml")
 
 #Set time function
 def set_time():
-    global current_time
     time_input = input("Enter the current time (HH:MM): ")
     try:
         # Validate and set the time
         hours, minutes = map(int, time_input.split(':'))
         if 0 <= hours < 24 and 0 <= minutes < 60:
             current_time = f"{hours:02d}:{minutes:02d}"
+            data = ET.parse("data.xml")
+            meta = data.getroot().find("meta")
+            current_time_element = meta.find("current_time")
+            current_time_element.text = current_time
+            data.write("data.xml")            
             print(f"Time set to {current_time}.")
         else:
             print("Invalid time format. Please enter a valid time.")
@@ -60,9 +62,14 @@ def display_menu():
     return choice
 
 #If the XML file does not exist, create it
+#If it does exist set system clock as current time
 try:
     with open("data.xml", "r") as file:
-        pass
+        data = ET.parse("data.xml")
+        meta = data.getroot().find("meta")
+        current_time_element = meta.find("current_time")
+        current_time_element.text = time.strftime("%H:%M")
+        data.write("data.xml")
 except FileNotFoundError:
     build_xml_file()
 
